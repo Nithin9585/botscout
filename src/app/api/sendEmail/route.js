7094a40ca241a1644f43433840a611fdd1ab511d
Nithin9585 <nithin958595@gmail.com>
2025-04-06 17:34:47 +0530

    .

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -1,31 +1,22 @@
-import { NextResponse } from "next/server";
-import { firestore } from "../../../../firebase/firebase";
-import { collection,getDocs } from "firebase/firestore";
-import { sendEmailsToSubscribers } from "@/lib/mail";
+import { render } from "@react-email/components";
+import index from "../../../../email";
+import { Resend } from "resend";
 
-export async function POST() {
-  try {
-    // Fetch all subscribers from Firestore
-    const subscribersSnapshot = await getDocs(collection(firestore, "subscribers"));
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    if (subscribersSnapshot.empty) {
-      return NextResponse.json({ success: false, message: "No subscribers found." });
+export async function POST(request, res){
+    const {email,userFirstname} = await request.json();
+
+    const {data,errror} = await resend.emails.send({
+        from:"Acme <onboarding@resend.dev>",
+        to:[email],
+        subject:"Thanklk u ",
+        html:<p>hi</p>
+        // navnit.23bce11827@vitbhopal.ac.in
+    });
+
+    if(error){
+        return Response.json(error);
     }
-
-    const subscribers = subscribersSnapshot.docs
-      .map((doc) => doc.data().email)
-      .filter((email) => email);
-
-    if (subscribers.length === 0) {
-      return NextResponse.json({ success: false, message: "No valid emails found." });
-    }
-
-    // Send emails using your mailing function
-    const response = await sendEmailsToSubscribers(subscribers);
-
-    return NextResponse.json({ success: true, data: response });
-  } catch (error) {
-    console.error("Error sending emails:", error);
-    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
-  }
+return Response.json({message:"email sent successfully"});
 }
