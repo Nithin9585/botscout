f46463fdef866ba02dfce8b683c67a67ec9dd227
Nithin9585 <nithin958595@gmail.com>
2025-04-06 09:48:19 +0530

    routes

---
+++ src/app/Arxivpages/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Arxivpages() {
+  return (
+    <div className='text-black
+    bg-white m-25'>
+
+        <h1>Arxiv pages</h1>
+        Arxivpages
+
+      
+    </div>
+  )
+}
+
+export default Arxivpages;
